7feb38477613723d4cfc4b7fd8f397226d3f5b9b
ankit <69717009+crew-guy@users.noreply.github.com>
2021-02-09 23:43:03 +0530

    learnt-webpack

---
+++ intro-webpack/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: "awesome.js",
+        path:path.resolve(__dirname,'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss?$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 5000
+    }
+}
